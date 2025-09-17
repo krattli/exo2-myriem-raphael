1b4702cf1b1ffa613c7773e2c07acb6b9d100f43
Raphael Krattli <raphael.krattli@etu-paris1.fr>
2025-09-17 16:26:23 +0200

    apps principales + routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { Homepage } from './homepage/homepage';
+import { Liste } from './liste/liste';
+import { Gestion } from './gestion/gestion';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Homepage },
+  { path: 'liste', component: Liste },
+  { path: 'gestion', component: Gestion },
+  { path: '**', redirectTo: '' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
