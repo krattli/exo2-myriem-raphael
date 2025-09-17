8505a6619d7d62d0296d7423addbc44aa4d258b9
Raphael Krattli <raphael.krattli@etu-paris1.fr>
2025-09-17 15:21:15 +0200

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
+import {Header} from './header/header';
+import { Footer } from "./footer/footer";
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
