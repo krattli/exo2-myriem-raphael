f57545af0c4d439cbb298112e5b7558a0af2f8a9
myr4j <myrtgnt@gmail.com>
2025-09-22 15:39:04 +0200

    form validators

--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -17,13 +17,21 @@ export class Contact {
     private router: Router
   ) { }
 
+  hideEmail : Boolean = false;
+
   onSubmit(form: NgForm) {
     if (form.valid) {
+      const email = form.value.email;
+      if (email && !email.includes('@')) {
+        alert("L'email doit contenir un @ !");
+        return;
+      }
       this.contactService.nom = form.value.nom;
       this.contactService.prenom = form.value.prenom;
       this.contactService.age = form.value.age;
       this.contactService.email = form.value.email;
       this.contactService.commentaire = form.value.commentaire;
+      alert("Le formulaire est valide !");
 
       this.router.navigate(['/']);
     }
