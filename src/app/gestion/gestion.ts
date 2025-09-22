9ee7bca979165f098d4308e1267281d085504be1
Raphael Krattli <raphael.krattli@etu-paris1.fr>
2025-09-22 12:27:59 +0200

    affichage des données contact dans page gestion

--- src/app/gestion/gestion.ts
+++ src/app/gestion/gestion.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ContactData } from '../contact-data';
 
 @Component({
   selector: 'Gestion',
@@ -7,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './gestion.css'
 })
 export class Gestion {
-
+  constructor(public contact_data: ContactData) {}
 }
