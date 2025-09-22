323edf8ef77ca8ef10a6a5d79c2a79b54f9bf39f
Raphael Krattli <raphael.krattli@etu-paris1.fr>
2025-09-22 09:57:36 +0200

    mise à jour du style (ne fonctionne tjr pas)

--- src/app/liste/liste.ts
+++ src/app/liste/liste.ts
@@ -6,6 +6,29 @@ import { Component } from '@angular/core';
   templateUrl: './liste.html',
   styleUrl: './liste.css'
 })
+
 export class Liste {
 
+  films: Film[] = [
+    new Film('Inception', 'assets/images/inception.jpg'),
+    new Film('Interstellar', 'assets/images/interstellar.jpg'),
+    new Film('The Dark Knight', 'assets/images/dark-knight.jpg'),
+    new Film('Matrix', 'assets/images/matrix.jpg'),
+    new Film('Le Seigneur des Anneaux', 'assets/images/lotr.jpg')
+  ];
+
+  selectedFilm: Film | null = null;
+
+  selectFilm(film: Film) {
+    this.selectedFilm = film;
+  }
+
+}
+
+class Film {
+
+  constructor(
+    public nom: string,
+    public affiche: string
+  ) {}
 }
