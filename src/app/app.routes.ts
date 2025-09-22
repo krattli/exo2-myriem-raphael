0e88a75fe3467286b228d9b15dc400bbab7195c1
Raphael Krattli <raphael.krattli@etu-paris1.fr>
2025-09-22 14:36:15 +0200

    error404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,15 @@ import { Homepage } from './homepage/homepage';
 import { Liste } from './liste/liste';
 import { Gestion } from './gestion/gestion';
 import { Contact } from './contact/contact';
+import { Error404 } from './error404/error404';
 
 export const routes: Routes = [
   { path: '', component: Homepage },
   { path: 'liste', component: Liste },
   { path: 'gestion', component: Gestion },
   { path: 'contact', component: Contact },
-  { path: '**', redirectTo: '' },
+  { path: 'error', component: Error404 },
+  { path: '**', component: Error404 },
 ];
 
 @NgModule({
--- src/app/gestion/gestion.ts
+++ src/app/gestion/gestion.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { ContactData } from '../services/contact-data';
+import { Error404 } from '../error404/error404';
 
 @Component({
   selector: 'Gestion',
-  imports: [],
+  imports: [Error404],
   templateUrl: './gestion.html',
   styleUrl: './gestion.css'
 })
 export class Gestion {
-  constructor(public contact_data: ContactData) {}
+  constructor(public contactData: ContactData) {}
 }
