3365b9faf9cc875544bfbd6dfd3f0dfd56906900
Raphael Krattli <raphael.krattli@etu-paris1.fr>
2025-09-22 16:42:26 +0200

    gestion On Init

--- src/app/gestion/gestion.ts
+++ src/app/gestion/gestion.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ContactData } from '../services/contact-data';
 import { Error404 } from '../error404/error404';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'Gestion',
@@ -8,6 +9,15 @@ import { Error404 } from '../error404/error404';
   templateUrl: './gestion.html',
   styleUrl: './gestion.css'
 })
-export class Gestion {
-  constructor(public contactData: ContactData) {}
+export class Gestion implements OnInit {
+  constructor(
+    public contactData: ContactData,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+      if (this.contactData.nom == null) {
+        this.router.navigate(['/error'])
+      }
+  }
 }
